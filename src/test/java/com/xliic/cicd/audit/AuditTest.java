e4cffa4218cf72f31aa8e1f5fd417e10ea5d6448
Anton Krasovsky <anton.krasovsky@gmail.com>
2021-03-26 19:49:39 +0000

    Update to use new dev platform

--- src/test/java/com/xliic/cicd/audit/AuditTest.java
+++ src/test/java/com/xliic/cicd/audit/AuditTest.java
@@ -19,8 +19,8 @@ public class AuditTest {
         Result result = auditor.getResult("multi-file-petstore/openapi.yaml");
         List<String> failures = Arrays.asList(result.failures);
 
-        assertEquals(result.score, 18);
-        assertTrue(failures.contains("The API score 18 is lower than the set minimum score of 75"));
+        assertEquals(result.score, 17);
+        assertTrue(failures.contains("The API score 17 is lower than the set minimum score of 75"));
         assertTrue(failures.contains("Found 35 issues with severity \"medium\" or higher"));
         assertTrue(failures.contains("Found issue \"v3-global-http-clear\""));
         assertTrue(failures.contains("Found issue \"v3-response-schema-undefined\""));
